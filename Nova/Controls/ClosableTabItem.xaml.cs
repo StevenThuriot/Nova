431c5b1e8794b5fa58a499f699a62b3849f4edb2
Steven Thuriot <steven@thuriot.be>
2012-11-29 18:36:24 +0100

    rollback so we can pick up on this behaviour later on (e.g. for handling other things when closing a tab)

--- Nova/Controls/ClosableTabItem.xaml.cs
+++ Nova/Controls/ClosableTabItem.xaml.cs
@@ -17,12 +17,8 @@
 // 
 
 #endregion
-using System.Collections;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
 
 namespace Nova.Controls
 {
@@ -30,12 +26,63 @@ namespace Nova.Controls
     /// Interaction logic for ClosableTabItem.xaml
     /// </summary>
     public partial class ClosableTabItem : TabItem
-    {        /// <summary>
+    {
+        /// <summary>
+        /// The close button
+        /// </summary>
+        private Button _CloseButton;
+
+        /// <summary>
+        /// The close tab event
+        /// </summary>
+        public static readonly RoutedEvent CloseTabEvent = EventManager.RegisterRoutedEvent("CloseTab", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(ClosableTabItem));
+
+        /// <summary>
+        /// Occurs when a tab item needs to be closed.
+        /// </summary>
+        public event RoutedEventHandler CloseTab
+        {
+            add { AddHandler(CloseTabEvent, value); }
+            remove { RemoveHandler(CloseTabEvent, value); }
+        }
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="ClosableTabItem" /> class.
         /// </summary>
         public ClosableTabItem()
         {
             InitializeComponent();
+            AddHandler(CloseTabEvent, new RoutedEventHandler(CloseCurrentTabItem));
+        }
+
+        /// <summary>
+        /// When overridden in a derived class, is invoked whenever application code or internal processes call <see cref="M:System.Windows.FrameworkElement.ApplyTemplate" />.
+        /// </summary>
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            if (_CloseButton != null)
+            {
+                _CloseButton.Click -= RaiseCloseEvent;
+            }
+
+            _CloseButton = GetTemplateChild("PART_CloseTab") as Button;
+            if (_CloseButton != null)
+            {
+                _CloseButton.Click += RaiseCloseEvent;
+            }
+        }
+
+        /// <summary>
+        /// Raises the close event.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs" /> instance containing the event data.</param>
+        private void RaiseCloseEvent(object sender, RoutedEventArgs e)
+        {
+            e.Handled = true;
+            RaiseEvent(new RoutedEventArgs(CloseTabEvent, this));
         }
 
         /// <summary>
@@ -43,38 +90,15 @@ namespace Nova.Controls
         /// </summary>
         /// <param name="sender">The sender.</param>
         /// <param name="e">The <see cref="RoutedEventArgs" /> instance containing the event data.</param>
-        private void CloseTabItem(object sender, RoutedEventArgs e)
+        private static void CloseCurrentTabItem(object sender, RoutedEventArgs e)
         {
-            TabControl tabControl = Parent as TabControl;
-            if (tabControl != null)
-            {
-                var items = (IEditableCollectionView)tabControl.Items;
-                if (items.CanRemove)
-                {
-                    items.Remove(this);
-                    return;
-                }
-            }
+            var tabItem = sender as TabItem;
+            if (tabItem == null) return;
 
-            if (tabControl == null)
-            {
-                DependencyObject previous = this;
-                for (int i = 0; i < 5; i++)
-                {
-                    previous = VisualTreeHelper.GetParent(previous);
-                    tabControl = previous as TabControl;
-                    if (tabControl != null) break;
-                }
-            }
+            var tabControl = tabItem.Parent as TabControl;
+            if (tabControl == null) return;
 
-            if (tabControl != null)
-            {
-                var items = tabControl.ItemsSource as IList;
-                if (items != null) //If null, the bound source is one that doesn't support removing items.
-                {
-                    items.Remove(this.DataContext);
-                }
-            }
+            tabControl.Items.Remove(tabItem);
         }
     }
 }
