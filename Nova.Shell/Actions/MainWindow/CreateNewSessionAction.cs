fcf8715579d3e4794718668d7f6abaa283efca11
Steven Thuriot <steven@thuriot.be>
2012-12-25 22:59:17 +0100

    Small tweaks

--- Nova.Shell/Actions/MainWindow/CreateNewSessionAction.cs
+++ Nova.Shell/Actions/MainWindow/CreateNewSessionAction.cs
@@ -1,13 +1,27 @@
-﻿using Nova.Base;
+﻿using System.Linq;
+using Nova.Base;
 
 namespace Nova.Shell.Actions.MainWindow
 {
     public class CreateNewSessionAction : BaseAction<MainView, MainViewModel>
     {
+        private bool _HasOpenSessions;
+
+        public override bool Execute()
+        {
+            _HasOpenSessions = ViewModel.Sessions.Any();
+            return true;
+        }
+
         public override void ExecuteCompleted()
         {
             var session = SessionView.Create(View, View._ActionQueueManager);
             ViewModel.Sessions.Add(session);
+
+            if (!_HasOpenSessions)
+            {
+                ViewModel.CurrentSession = session;
+            }
         }
     }
 }
