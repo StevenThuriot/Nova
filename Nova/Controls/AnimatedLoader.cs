b1f8dee84aa93ec45d31a94f897997feeee5d050
Steven Thuriot <steven@thuriot.be>
2013-03-31 22:17:42 +0200

    Cleaned up a little

--- Nova/Controls/AnimatedLoader.cs
+++ Nova/Controls/AnimatedLoader.cs
@@ -1,4 +1,5 @@
 ﻿#region License
+
 // 
 //  Copyright 2012 Steven Thuriot
 // 
@@ -14,7 +15,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 // 
+
 #endregion
+
 using System;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,104 +27,110 @@ using System.Windows.Media.Imaging;
 
 namespace Nova.Controls
 {
-	/// <summary>
-	/// Class to show an animated loading image.
-	/// </summary>
-	public class AnimatedLoader : Image
-	{
-		/// <summary>
-		/// The frame index.
-		/// </summary>
-		public static readonly DependencyProperty FrameIndexProperty =
-			DependencyProperty.Register("FrameIndex", typeof (int), typeof (AnimatedLoader), new PropertyMetadata(0, ChangingFrameIndex));
+    /// <summary>
+    ///     Class to show an animated loading image.
+    /// </summary>
+    public class AnimatedLoader : Image
+    {
+        /// <summary>
+        ///     The frame index.
+        /// </summary>
+        public static readonly DependencyProperty FrameIndexProperty =
+            DependencyProperty.Register("FrameIndex", typeof (int), typeof (AnimatedLoader), new PropertyMetadata(0, ChangingFrameIndex));
 
-		private readonly Int32Animation _Animation;
-		private readonly GifBitmapDecoder _GifBitmapDecoder;
+        private readonly Int32Animation _Animation;
+        private readonly GifBitmapDecoder _GifBitmapDecoder;
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="AnimatedLoader"/> class.
-		/// </summary>
-		public AnimatedLoader()
-		{
-			Visibility = Visibility.Hidden;
-			IsVisibleChanged += LoaderVisibilityChanged;
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="AnimatedLoader" /> class.
+        /// </summary>
+        public AnimatedLoader()
+        {
+            Visibility = Visibility.Hidden;
+            IsVisibleChanged += LoaderVisibilityChanged;
 
-			var imageSource = Application.Current.Resources["LoaderImage"] as ImageSource;
-			Uri loaderUri = imageSource == null
-			                	? new Uri("pack://application:,,,/Nova;component/Resources/loader.gif", UriKind.Absolute)
-			                	: new Uri(imageSource.ToString(), UriKind.Absolute);
+            var imageSource = Application.Current.Resources["LoaderImage"] as ImageSource;
+            var loaderUri = imageSource == null
+                                ? new Uri("pack://application:,,,/Nova;component/Resources/loader.gif", UriKind.Absolute)
+                                : new Uri(imageSource.ToString(), UriKind.Absolute);
 
-			_GifBitmapDecoder = new GifBitmapDecoder(loaderUri, BitmapCreateOptions.PreservePixelFormat,
-			                                         BitmapCacheOption.Default);
-			var timespan = new TimeSpan(0, 0, 0, _GifBitmapDecoder.Frames.Count/10,
-			                            (int)
-			                            (((_GifBitmapDecoder.Frames.Count/10.0) - (_GifBitmapDecoder.Frames.Count/10))*1000));
-			var duration = new Duration(timespan);
+            _GifBitmapDecoder = new GifBitmapDecoder(loaderUri, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
 
-			_Animation = new Int32Animation(0, _GifBitmapDecoder.Frames.Count - 1, duration)
-			             	{
-			             		RepeatBehavior = RepeatBehavior.Forever
-			             	};
+            var seconds = _GifBitmapDecoder.Frames.Count/10;
+            var milliseconds = (int) (((_GifBitmapDecoder.Frames.Count/10.0) - (_GifBitmapDecoder.Frames.Count/10.0))*1000d);
 
-			Source = _GifBitmapDecoder.Frames[0];
-			Height = Source.Height;
-			Width = Source.Width;
-		}
+            var timespan = new TimeSpan(0, 0, 0, seconds, milliseconds);
+            var duration = new Duration(timespan);
 
-		/// <summary>
-		/// Gets or sets the index of the frame.
-		/// </summary>
-		/// <value>
-		/// The index of the frame.
-		/// </value>
-		public int FrameIndex
-		{
-			get { return (int) GetValue(FrameIndexProperty); }
-			set { SetValue(FrameIndexProperty, value); }
-		}
+            _Animation = new Int32Animation(0, _GifBitmapDecoder.Frames.Count - 1, duration)
+                {
+                    RepeatBehavior = RepeatBehavior.Forever
+                };
 
-		/// <summary>
-		/// Caleed when the frameindex changed.
-		/// </summary>
-		/// <param name="dependencyObject">The dependencyObject.</param>
-		/// <param name="eventArguments">The <see cref="System.Windows.DependencyPropertyChangedEventArgs"/> instance containing the event data.</param>
-		private static void ChangingFrameIndex(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs eventArguments)
-		{
-			var ob = dependencyObject as AnimatedLoader;
-			if (ob == null) return;
+            Source = _GifBitmapDecoder.Frames[0];
+            Height = Source.Height;
+            Width = Source.Width;
+        }
 
-			ob.Source = ob._GifBitmapDecoder.Frames[(int) eventArguments.NewValue];
-			ob.InvalidateVisual();
-		}
+        /// <summary>
+        ///     Gets or sets the index of the frame.
+        /// </summary>
+        /// <value>
+        ///     The index of the frame.
+        /// </value>
+        public int FrameIndex
+        {
+            get { return (int) GetValue(FrameIndexProperty); }
+            set { SetValue(FrameIndexProperty, value); }
+        }
 
-		/// <summary>
-		/// Starts animating.
-		/// </summary>
-		public void Start()
-		{
-			Visibility = Visibility.Visible;
-		}
+        /// <summary>
+        ///     Caleed when the frameindex changed.
+        /// </summary>
+        /// <param name="dependencyObject">The dependencyObject.</param>
+        /// <param name="eventArguments">
+        ///     The <see cref="System.Windows.DependencyPropertyChangedEventArgs" /> instance containing the event data.
+        /// </param>
+        private static void ChangingFrameIndex(DependencyObject dependencyObject,
+                                               DependencyPropertyChangedEventArgs eventArguments)
+        {
+            var ob = dependencyObject as AnimatedLoader;
+            if (ob == null) return;
 
-		/// <summary>
-		/// Stops animating.
-		/// </summary>
-		public void Stop()
-		{
-			Visibility = Visibility.Hidden;
-		}
+            ob.Source = ob._GifBitmapDecoder.Frames[(int) eventArguments.NewValue];
+            ob.InvalidateVisual();
+        }
 
-		/// <summary>
-		/// Called when the loaders visibility has changed.
-		/// </summary>
-		/// <param name="sender">The sender.</param>
-		/// <param name="dependencyPropertyChangedEventArgs">The <see cref="System.Windows.DependencyPropertyChangedEventArgs"/> instance containing the event data.</param>
-		private void LoaderVisibilityChanged(object sender,
-		                                     DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
-		{
-			bool isVisible = Visibility == Visibility.Visible;
-			Int32Animation animation = isVisible ? _Animation : null;
+        /// <summary>
+        ///     Starts animating.
+        /// </summary>
+        public void Start()
+        {
+            Visibility = Visibility.Visible;
+        }
 
-			BeginAnimation(FrameIndexProperty, animation);
-		}
-	}
+        /// <summary>
+        ///     Stops animating.
+        /// </summary>
+        public void Stop()
+        {
+            Visibility = Visibility.Hidden;
+        }
+
+        /// <summary>
+        ///     Called when the loaders visibility has changed.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="dependencyPropertyChangedEventArgs">
+        ///     The <see cref="System.Windows.DependencyPropertyChangedEventArgs" /> instance containing the event data.
+        /// </param>
+        private void LoaderVisibilityChanged(object sender,
+                                             DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            var isVisible = Visibility == Visibility.Visible;
+            var animation = isVisible ? _Animation : null;
+
+            BeginAnimation(FrameIndexProperty, animation);
+        }
+    }
 }
