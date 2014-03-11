8ecd031af133fcc5a6387f5723a1f481896c44c3
Steven Thuriot <steven@thuriot.be>
2014-03-11 19:51:34 +0100

    Added informational version to shell lib

--- Nova.Shell.Library/Properties/AssemblyInfo.cs
+++ Nova.Shell.Library/Properties/AssemblyInfo.cs
@@ -29,3 +29,4 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(true)]
 [assembly: Guid("87ac3ac4-240d-4d53-bf22-59934c261af7")]
 [assembly: InternalsVisibleTo("Nova.Shell")]
+[assembly: AssemblyInformationalVersion("0.8.0")]
